350c1491f7a12d8b288629321f800381d17449f4
Talkmore2008 <111118575+Talkmore2008@users.noreply.github.com>
2025-08-04 19:51:01 +0200

    ctrl

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~dm8iFSgY#Q5QJp1BxIpF9txga-jFYYHrm649oKWYmmIejfvI9tbE",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "263788513905", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
